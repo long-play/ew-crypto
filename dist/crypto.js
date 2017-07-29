1cb5213a56c14262cd99009960cd68cf8dbd9991
Eugene Valeyev <evgen.povt@gmail.com>
2017-07-29 18:25:39 +0300

    Added WCrypto class

--- dist/crypto.js
+++ dist/crypto.js
@@ -502,103 +502,193 @@ class CryptoRSA {
   }
 }
 
-class Crypto {
-  constructor(rsaLength = 2048, aesLength = 256) {
-    this.rsa = new CryptoRSA();
-    this.aes = new CryptoAES();
-    this.rsaLength = rsaLength;
-    this.aesLength = aesLength;
+const BN = require('bn.js');
+const EC = require('elliptic').ec;
+const createHash = require('create-hash');
+
+const ec = new EC('secp256k1');
+const ecparams = ec.curve;
+
+class WCrypto {
+  constructor() {
   }
 
-  generateKeys() {
-    return this.rsa.generateKeys(this.rsaLength);
+  encrypt(msg, privKeyFrom, pubKeyTo) {
+    const privateKeyFrom = this._privateKeyCreateFromHex(privKeyFrom);
+    const publicKeyFrom = this._getPublicKeyFromPrivate(privKeyFrom);
+    const publicKeyTo = this._getPublicKey(pubKeyTo);
+
+    const Px = this._derive(publicKeyTo, privateKeyFrom);
+    const hash = this._sha256(Px);
+    const encrypted = this._aesEncrypt(msg, hash.toString('hex'));
+    //const encrypted = 'aes256gcm.encrypt(iv, hash, msg)';
+    return encrypted;
   }
 
-  importKeys(privateKey, publicKey) {
-    return this.rsa.importKeys(privateKey, publicKey);
+  decrypt(msg, privKeyTo, pubKeyFrom, iv) {
+    const privateKeyTo = this._privateKeyCreateFromHex(privKeyTo);
+    const publicKeyTo = this._getPublicKeyFromPrivate(privKeyTo);
+    const publicKeyFrom = this._getPublicKey(pubKeyFrom);
+
+    const Px = this._derive(publicKeyFrom, privateKeyTo);
+    const hash = this._sha256(Px);
+    const decrypted = this._aesDecrypt(msg, hash.toString('hex'), iv);
+    //const decrypted = 'aes256gcm.decrypt(iv, hash, msg)';
+    return decrypted;
   }
 
-  exportKeys() {
-    return this.rsa.exportKeys();
-  }
-
-  encrypt(textData) {
-    let encryptedData = null;
-    let encryptedKey = null;
-    const textData64 = this._encode64(textData);
-
-    const promise = this.aes.generateKeys(this.aesLength).then( (aes) => {
-      return this.aes.exportKeys();
-    }).then( (aes) => {
-      return this.aes.encrypt(textData64);
+  // private functions
+  _aesEncrypt(msg, key) {
+    const aes = new CryptoAESCBC();
+    const promise = aes.createKeyFromHex(key).then( (aes) => {
+      console.log('key: ' + aes.key);
+      console.log('iv: ' + aes.iv);
+      return aes.encrypt(msg);
     }).then( (encrypted) => {
-      const key = this._composeKeyIV(this.aes.key, this.aes.iv);
-      encryptedData = encrypted;
-      return this.rsa.encrypt(key);
-    }).then( (encrypted) => {
-      encryptedKey = encrypted;
-      return Promise.resolve(this._composeEncryptedKeyData(encryptedKey, encryptedData));
+      console.log('encr: ' + encrypted);
+      return Promise.resolve({ encrypted: encrypted, iv: aes.iv });
     });
     return promise;
   }
 
-  decrypt(encryptedTextData) {
-    const encryptedKey = this._extractEncryptedKey(encryptedTextData);
-    const encryptedData = this._extractEncryptedData(encryptedTextData);
-
-    const promise = this.rsa.decrypt(encryptedKey).then( (decryptedKey) => {
-      const aesIV = this._extractAESIV(decryptedKey);
-      const aesKey = this._extractAESKey(decryptedKey);
-      return this.aes.importKeys(aesKey, aesIV);
-    }).then( (aes) => {
-      return this.aes.decrypt(encryptedData);
-    }).then( (decrypted64) => {
-      const decrypted = this._decode64(decrypted64);
+  _aesDecrypt(msg, key, iv) {
+    const aes = new CryptoAESCBC();
+    const hexIV = CryptoUtil.base64ToHex(iv);
+    const promise = aes.createKeyFromHex(key, hexIV).then( (aes) => {
+      console.log('key: ' + aes.key);
+      console.log('iv: ' + aes.iv);
+      return aes.decrypt(msg);
+    }).then( (decrypted) => {
+      console.log('decr: ' + decrypted);
       return Promise.resolve(decrypted);
     });
     return promise;
   }
 
-  // private methods
-  _encode64(text) {
-    return text;
+  _getPublicKey(pubKey) {
+    //assert(privKey.length === 32, "Bad private key");
+    // See https://github.com/wanderer/secp256k1-node/issues/46
+    var compressed = this._publicKeyCreateFromHex(pubKey);
+    return this._publicKeyConvert(compressed, false);
   }
 
-  _decode64(text64) {
-    return text64;
+  _getPublicKeyFromPrivate(privKey) {
+    //assert(privKey.length === 32, "Bad private key");
+    // See https://github.com/wanderer/secp256k1-node/issues/46
+    var compressed = this._publicKeyCreateFromPrivate(privKey);
+    return this._publicKeyConvert(compressed, false);
   }
 
-  _extractEncryptedKey(keyData) {
-    const idx = keyData.indexOf(' ');
-    return keyData.substring(0, idx);
+  _derive(pubA, privB) {
+    return this._ecdh(pubA, privB);
   }
 
-  _extractEncryptedData(keyData) {
-    const idx = keyData.indexOf(' ');
-    return keyData.substring(idx + 1);
+  _sha256(msg) {
+    return createHash('sha256').update(msg).digest();
   }
 
-  _composeEncryptedKeyData(key, data) {
-    return `${key} ${data}`;
+  _sha512(msg) {
+    return createHash('sha512').update(msg).digest();
   }
 
-  _extractAESIV(key) {
-    const idx = key.indexOf(' ');
-    return key.substring(0, idx);
+  // secp256k1
+  _privateKeyCreateFromHex(privKey) {
+    //const d = new BN(privKey)
+    //if (d.cmp(ecparams.n) >= 0 || d.isZero()) throw new Error(messages.EC_PUBLIC_KEY_CREATE_FAIL)
+
+    //const priv = ec.keyFromPrivate(privKey, 'hex').getPrivate();
+    return Buffer.from(privKey, 'hex'); //ec.keyFromPrivate(privKey).getPrivate())
   }
 
-  _extractAESKey(key) {
-    const idx = key.indexOf(' ');
-    return key.substring(idx + 1);
+  _publicKeyCreateFromHex(pubKey, compressed) {
+    //const d = new BN(privKey)
+    //if (d.cmp(ecparams.n) >= 0 || d.isZero()) throw new Error(messages.EC_PUBLIC_KEY_CREATE_FAIL)
+
+    return Buffer.from(ec.keyFromPublic(pubKey, 'hex').getPublic(compressed, true))
   }
 
-  _composeKeyIV(key, iv) {
-    return `${iv} ${key}`;
+  _publicKeyCreateFromPrivate(privKey, compressed) {
+    //const d = new BN(privKey)
+    //if (d.cmp(ecparams.n) >= 0 || d.isZero()) throw new Error(messages.EC_PUBLIC_KEY_CREATE_FAIL)
+
+    return Buffer.from(ec.keyFromPrivate(privKey).getPublic(compressed, true))
+  }
+
+  _publicKeyConvert(pubKey, compressed) {
+    const pair = this._loadPublicKey(pubKey)
+    if (pair === null) throw new Error(messages.EC_PUBLIC_KEY_PARSE_FAIL)
+
+    return Buffer.from(pair.getPublic(compressed, true))
+  }
+
+  _loadPublicKey(pubKey) {
+    const first = pubKey[0]
+    switch (first) {
+      case 0x02:
+      case 0x03:
+        if (pubKey.length !== 33) return null
+        return this._loadCompressedPublicKey(first, pubKey.slice(1, 33))
+      case 0x04:
+      case 0x06:
+      case 0x07:
+        if (pubKey.length !== 65) return null
+        return this._loadUncompressedPublicKey(first, pubKey.slice(1, 33), pubKey.slice(33, 65))
+      default:
+        return null
+    }
+  }
+
+  _loadCompressedPublicKey (first, xBuffer) {
+    let x = new BN(xBuffer)
+
+    // overflow
+    if (x.cmp(ecparams.p) >= 0) return null
+    x = x.toRed(ecparams.red)
+
+    // compute corresponding Y
+    const y = x.redSqr().redIMul(x).redIAdd(ecparams.b).redSqrt()
+    if ((first === 0x03) !== y.isOdd()) y = y.redNeg()
+
+    return ec.keyPair({ pub: { x: x, y: y } })
+  }
+
+  _loadUncompressedPublicKey (first, xBuffer, yBuffer) {
+    let x = new BN(xBuffer)
+    let y = new BN(yBuffer)
+
+    // overflow
+    if (x.cmp(ecparams.p) >= 0 || y.cmp(ecparams.p) >= 0) return null
+
+    x = x.toRed(ecparams.red)
+    y = y.toRed(ecparams.red)
+
+    // is odd flag
+    if ((first === 0x06 || first === 0x07) && y.isOdd() !== (first === 0x07)) return null
+
+    // x*x*x + b = y*y
+    const x3 = x.redSqr().redIMul(x)
+    if (!y.redSqr().redISub(x3.redIAdd(ecparams.b)).isZero()) return null
+
+    return ec.keyPair({ pub: { x: x, y: y } })
+  }
+
+  _ecdh (publicKey, privateKey) {
+    var shared = this._ecdhUnsafe(publicKey, privateKey, true)
+    return this._sha256(shared);
+  }
+
+  _ecdhUnsafe (publicKey, privateKey, compressed) {
+    var pair = this._loadPublicKey(publicKey)
+    //if (pair === null) throw new Error(messages.EC_PUBLIC_KEY_PARSE_FAIL)
+
+    var scalar = new BN(privateKey)
+    //if (scalar.cmp(ecparams.n) >= 0 || scalar.isZero()) throw new Error(messages.ECDH_FAIL)
+
+    return Buffer.from(pair.pub.mul(scalar).encode(true, compressed))
   }
 }
-const cu = new CryptoUtil();
-
 exports.AESCBC = CryptoAESCBC;
 exports.AESGCM = CryptoAESGCM;
 exports.RSA = CryptoRSA;
 exports.Util = CryptoUtil;
+exports.WCrypto = WCrypto;
--- src/export.js
+++ src/export.js
@@ -1,6 +1,5 @@
-const cu = new CryptoUtil();
-
 exports.AESCBC = CryptoAESCBC;
 exports.AESGCM = CryptoAESGCM;
 exports.RSA = CryptoRSA;
 exports.Util = CryptoUtil;
+exports.WCrypto = WCrypto;
