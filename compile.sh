3c35355b95e584ee4830ce2bc2d78191f7336942
Eugene Valeyev <evgen.povt@gmail.com>
2017-07-30 13:36:33 +0300

    Renamed Crypto -> WCrypto

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat src/util.js src/cbc.js src/gcm.js src/rsa.js src/wcrypto.js src/export.js > dist/crypto.js
-uglifyjs --compress -o dist/crypto.min.js dist/crypto.js
+cat src/util.js src/cbc.js src/gcm.js src/rsa.js src/wcrypto.js src/export.js > dist/wcrypto.js
+uglifyjs --compress -o dist/wcrypto.min.js dist/wcrypto.js
 
